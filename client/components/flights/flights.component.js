9b5be2354801880671a2fd6d2f26c3bdd93d9381
hellajeffrey <Jeffreyhenneberg@gmail.com>
2017-07-10 18:01:12 -0400

    setting up components

---
+++ client/components/flights/flights.component.js
@@ -0,0 +1,9 @@
+import flightsComponent from "./flights.controller";
+import flightsTemplate from "./flights.html";
+
+const flightsComponent = {
+  controller: flightsController,
+  template: flightsTemplate
+};
+
+angular.module("TravelApp").component("flights", flightsComponent);
