920f572762744960b09c0758739b0c4c54de65f4
hellajeffrey <Jeffreyhenneberg@gmail.com>
2017-07-11 11:15:29 -0400

    npm install angular UI-router and corrected syntax errors

--- client/components/flights/index/flights.controller.js
+++ client/components/flights/index/flights.controller.js
@@ -14,17 +14,6 @@ function FlightsController(flightsService) {
   activate();
 
   function activate() {
-    vm.artists = [
-      {
-        name: "Test artist",
-        photo_url: "http://www.fillmurray.com/200/200",
-        nationality: "USA"
-      }, {
-        name: "Test artist 2",
-        photo_url: "http://www.fillmurray.com/205/205",
-        nationality: "USA"
-      }
-    ]
   }
 
   function getFlight() {
@@ -37,4 +26,9 @@ function FlightsController(flightsService) {
            });
   }
 }
-export default ArtistsController;
+
+angular
+  .module("TravelApp")
+  .controller("flightsController", FlightsController);
+
+export default FlightsController;
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
     mount_devise_token_auth_for 'User', at: 'auth'
     resources :flights do
+    end
 
     root "welcome#index"
-  end
+
 end
