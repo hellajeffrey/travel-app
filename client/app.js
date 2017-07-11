f604fef47cb87e65cbd2956a42b977ab9f68fa0b
katemontha <katemontha@gmail.com>
2017-07-11 16:22:19 -0400

    fixing inject error for heroku

--- client/app.js
+++ client/app.js
@@ -4,13 +4,15 @@ require("ng-token-auth");
 require("angular-cookie");
 
 angular.module('TravelApp', ["ui.router", 'ng-token-auth'])
-    .config(function($authProvider) {
-        $authProvider.configure({
-            apiUrl: '/'
-        });
-    })
-    .config(router);
+    .config(auth, router);
 
+auth.$inject = ["$authProvider"]
+
+function auth($authProvider) {
+    $authProvider.configure({
+        apiUrl: '/'
+    });
+}
 router.$inject = ["$stateProvider", "$urlRouterProvider"];
 
 function router($stateProvider, $urlRouterProvider) {
