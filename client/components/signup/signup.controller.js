85747ae1264ff4f3f9c529d23fcfbc2695ebdc42
katemontha <katemontha@gmail.com>
2017-07-12 11:03:44 -0400

    indentation and consistently naming signup variables

--- client/components/signup/signup.controller.js
+++ client/components/signup/signup.controller.js
@@ -1,33 +1,33 @@
 signUpController.$inject = ['$auth'];
 
 function signUpController($auth) {
-    const vm = this;
-    vm.registrationForm = {};
-    vm.signUp = signUp;
-
-    function signUp() {
-        $auth.submitRegistration(vm.registrationForm)
-            .then(function(resp) {
-                // handle success response
-
-            })
-            .catch(function(resp) {
-                // handle error response
-            });
-    }
+  const vm = this;
+  vm.registrationForm = {};
+  vm.signUp = signUp;
+  
+  function signUp() {
+    $auth.submitRegistration(vm.registrationForm)
+    .then(function(resp) {
+      $state.go('/flights')
+      
+    })
+    .catch(function(resp) {
+      alert('error')
+    });
+  }
 }
 
 export default signUpController;
 
 // angular.module('ngTokenAuthTestApp')
 //     .controller('IndexCtrl', function($scope, $auth) {
-//         $scope.handleRegBtnClick = function() {
-//             $auth.submitRegistration($scope.registrationForm)
-//                 .then(function(resp) {
-//                     // handle success response
-//                 })
-//                 .catch(function(resp) {
-//                     // handle error response
-//                 });
-//         };
-//     });
+  //         $scope.handleRegBtnClick = function() {
+    //             $auth.submitRegistration($scope.registrationForm)
+    //                 .then(function(resp) {
+      //                     // handle success response
+      //                 })
+      //                 .catch(function(resp) {
+        //                     // handle error response
+        //                 });
+        //         };
+        //     });
